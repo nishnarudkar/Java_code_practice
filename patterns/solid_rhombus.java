1142c2b347bd9030d9fef1e0cc907d607895fd27
Nishant Narudkar <nishnarudkar@gmail.com>
2026-06-07 15:21:35 +0530

    Solid rhombus practice code

---
+++ patterns/solid_rhombus.java
@@ -0,0 +1,17 @@
+package patterns;
+
+public class solid_rhombus {
+    public static void main(String args[]){
+        int n = 5;
+        for(int i =1; i<=n;i++){
+            int space = n-i;
+            for(int j =1; j<=space;j++){
+                System.out.print(" ");
+            }
+            for(int j =n; j>=1;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}

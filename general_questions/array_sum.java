60b44ddf4cba7a5a055bc7e38f3ccbc8bb027d61
Nishant Narudkar <nishnarudkar@gmail.com>
2026-06-08 20:49:39 +0530

    Sum of the Array code

--- general_questions/array_sum.java
+++ general_questions/array_sum.java
@@ -1,4 +1,7 @@
-public class reverse_arr {
+package general_questions;
+
+
+public class array_sum {
     public static void main(String [] args){
         int x[] = new int[]{10,20,30,40,50};
         int sum = 0;

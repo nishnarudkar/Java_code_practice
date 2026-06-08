a702b3dfab85e008246598527ed2200ad8820acb
Nishant Narudkar <nishnarudkar@gmail.com>
2026-06-08 23:16:57 +0530

    Removing spaces in the string

--- general_questions/remove_spaces.java
+++ general_questions/remove_spaces.java
@@ -1,5 +1,9 @@
 package general_questions;
 
 public class remove_spaces {
-    
+    public static void main(String [] args){
+        String s1= " Nishant is a good boy";
+        s1 = s1.replace(" ","");
+        System.out.println(s1);
+    }
 }

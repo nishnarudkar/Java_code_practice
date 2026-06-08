5e0874aa43320bdfd307ffa9570c0e2f3e85e59a
Nishant Narudkar <nishnarudkar@gmail.com>
2026-06-08 23:10:40 +0530

    Count the digits in the string code

--- general_questions/count_digits.java
+++ general_questions/count_digits.java
@@ -1,6 +1,17 @@
+package general_questions;
+
+
 public class count_digits {
     public static void main(String args[]){
-        String s1 = "hello student123";
-        String ch[] arr = 
+        String s1 = "hello4556 student123";
+        int count = 0;
+        char arr[] = s1.toCharArray();
+        for(char ch:arr){
+            if(Character.isDigit(ch)){
+                count++;
+            }
+        }
+
+        System.out.print("The digits in the String are: "+count);
     }
 }

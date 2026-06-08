9232368d3d1ab6dba7dcc41cb7a4d4e461543f45
Nishant Narudkar <nishnarudkar@gmail.com>
2026-06-08 22:46:03 +0530

    String traversing

--- general_questions/string_traverse.java
+++ general_questions/string_traverse.java
@@ -1,5 +1,21 @@
 package general_questions;
 
 public class string_traverse {
-    
+    public static void main(String[] args){
+        String s1= "Nishant is good";
+        char arr[]= s1.toCharArray();
+        for(char w:arr){
+            System.out.print(w);
+
+        }
+    }
+
+
+
+
+
+
+
+
+
 }

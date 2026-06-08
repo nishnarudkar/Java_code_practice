3f8af835a58975ae534d9487133f3195ef7a6577
Nishant Narudkar <nishnarudkar@gmail.com>
2026-06-08 23:21:34 +0530

    Calculating AsCII value for each character

---
+++ general_questions/ascii_values.java
@@ -0,0 +1,12 @@
+package general_questions;
+
+public class ascii_values{
+    public static void main(String[] args){
+        String s1= "Nishant";
+        for(int i =0; i<s1.length();i++){
+            char ch = s1.charAt(i);
+
+            System.out.println(ch + " ->"+ (int)ch);
+        }
+    }
+}

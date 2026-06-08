9f88a3bf563b7943d9556b325cea3c6a5907af9f
Nishant Narudkar <nishnarudkar@gmail.com>
2026-06-08 21:08:29 +0530

    Reverse a string code

--- general_questions/string_reverse.java
+++ general_questions/string_reverse.java
@@ -1,5 +1,12 @@
 package general_questions;
 
 public class string_reverse {
+    public static void main(String[] args){
+    String s1 = "Nishant is good";
+
+    String s2 = new StringBuilder(s1).reverse().toString();
+    System.out.print(s2);
+
+    }
     
 }

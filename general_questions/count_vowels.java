588aa574b0c5d1656e2b6bee80cbfba70d0f43ba
Nishant Narudkar <nishnarudkar@gmail.com>
2026-06-08 23:00:45 +0530

    Vowel count code

--- general_questions/count_vowels.java
+++ general_questions/count_vowels.java
@@ -1,5 +1,16 @@
 package general_questions;
 
 public class count_vowels {
-    
+    public static void main(String[] args){
+    String s1 = "Nishant is good";
+    char arr[]= s1.toLowerCase().toCharArray();
+    int vowels = 0;
+    for(char ch:arr){
+        if(ch=='a'|| ch == 'e'|| ch=='i'|| ch=='o' || ch=='u'){
+            vowels++;
+        }
+
+    }
+    System.out.print("Total vowels in the String are:"+vowels);
+}
 }

37f4b6309c1ece982869acb4083ae10b9343575e
Nishant Narudkar <nishnarudkar@gmail.com>
2026-06-08 21:04:05 +0530

    Palindrome code

--- general_questions/palindrome.java
+++ general_questions/palindrome.java
@@ -1,5 +1,16 @@
 package general_questions;
 
 public class palindrome {
-    
+    public static void main(String[] args){
+    String s1 = "wow";
+
+    String reversed = new StringBuilder(s1).reverse().toString();
+    if(s1.equals(reversed)){
+        System.out.print("It is palindrome");
+    }
+    else{
+        System.out.print("It is not a palindrome");
+    }
+
+  }
 }

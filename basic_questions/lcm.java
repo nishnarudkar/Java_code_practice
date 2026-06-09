6bdbdf7e49fd88835a37fe6519cc058c6be387de
Nishant Narudkar <nishnarudkar@gmail.com>
2026-06-10 02:46:33 +0530

    LCM of two numbers code

--- basic_questions/lcm.java
+++ basic_questions/lcm.java
@@ -1,5 +1,21 @@
 package basic_questions;
-
+import java.util.Scanner;
 public class lcm {
-    
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number a : ");
+        int a = sc.nextInt();
+        System.out.print("Enter the number b: ");
+        int b = sc.nextInt();
+        int lcm = Math.max(a,b);
+        while(true){
+            if (lcm %a == 0 && lcm %b ==0){
+                break;
+            }
+            lcm++;
+        }
+        System.out.print("The Lcm of two number is : "+ lcm);
+        sc.close();
+
+    }
 }

0ddd459c5344044de6b959efc5999155005cd7de
Nishant Narudkar <nishnarudkar@gmail.com>
2026-06-10 02:27:49 +0530

    Reverse the number

--- basic_questions/reverse_number.java
+++ basic_questions/reverse_number.java
@@ -1,27 +1,21 @@
+package basic_questions;
 import java.util.Scanner;
 
-public class reverse_number {
-
-    static int rev(int no) {
-        int r = 0;
-
-        while (no > 0) {
-            int ld = no % 10;
-            r = r * 10 + ld;
-            no = no / 10;
-        }
-
-        return r;
-    }
-
-    public static void main(String[] args) {
+public class reverse_number{
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter a number: ");
-        int no = sc.nextInt();
-
-        System.out.println("Reversed number: " + rev(no));
-
+        System.out.print("Enter the number: ");
+        int num = sc.nextInt();
+        int reverse = 0;
+        int original = num;
+        while(num>0){
+            int digits = num%10;
+            reverse = reverse*10 + digits;
+            num = num/10;
+        }
+        System.out.print("The reverse number of "+ original+ " is :"+ reverse);
         sc.close();
+
+
     }
 }

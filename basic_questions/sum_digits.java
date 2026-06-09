ee3aba72a2e258d5be854eaeb3304420e045efcc
Nishant Narudkar <nishnarudkar@gmail.com>
2026-06-10 02:36:08 +0530

    Sum of the digits code

--- basic_questions/sum_digits.java
+++ basic_questions/sum_digits.java
@@ -1,3 +1,19 @@
+package basic_questions;
+
+import java.util.Scanner;
 public class sum_digits {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int num = sc.nextInt();
+        int sum = 0;
+        while(num>0){
+            int digits = num%10;
+            sum += digits;
+            num = num/10;
+        }
+        System.out.print("The sum of the digits is : " + sum);
+        sc.close();
+    }
     
 }

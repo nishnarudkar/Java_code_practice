ca57dfa95533c2279748165189b08ea4f138b6a1
Nishant Narudkar <nishnarudkar@gmail.com>
2026-06-10 02:20:11 +0530

    Product of digits code

--- basic_questions/product_digits.java
+++ basic_questions/product_digits.java
@@ -1,3 +1,5 @@
+package basic_questions;
+
 import java.util.Scanner;
 
 
@@ -5,5 +7,24 @@ import java.util.Scanner;
 public class product_digits {
     public static void main(String [] args){
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number");
+        int num = sc.nextInt();
+        if(num == 0){
+            System.out.print("The product of digits is 0");
+        }
+        else{
+            num = Math.abs(num);
+            int product = 1;
+        while(num>0){
+            int digits = num%10;
+            product*= digits;
+            num = num/10;
+        }
+        
+         System.out.print("The product of digits is : "+ product);
+        }
+       
+        sc.close();
+
     }
 }
